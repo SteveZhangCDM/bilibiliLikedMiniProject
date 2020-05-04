94cdf921a567e32ad7459602cdcd7fe5770da1f3
Steve_Zhang <wentaozhang0305@gmail.com>
2020-05-04 14:54:11 -0700

    created scroll navigator and request the data

--- pages/mainpage/mainpage.js
+++ pages/mainpage/mainpage.js
@@ -1,8 +1,31 @@
 //Page Object
 Page({
-  data: {},
+  data: {
+    //main page navigator data
+    navList: [],
+  },
+  // 获取首页导航数据
+  getNavList() {
+    let that = this;
+    //小程序内置请求方法
+    wx.request({
+      url:
+        'https://mockapi.eolinker.com/7b7NMB9c75d613bc39c8f16e4e03a3d4a8f951750079dc5/navList',
+      success(res) {
+        console.log(res);
+        if (res.data.code === 0) {
+          that.setData({
+            navList: res.data.data.navList,
+          });
+        }
+      },
+    });
+  },
   //options(Object)
-  onLoad: function (options) {},
+  onLoad: function (options) {
+    //获取首页导航数据
+    this.getNavList();
+  },
   onReady: function () {},
   onShow: function () {},
   onHide: function () {},
